cb929ca692853ae3610b3c67153c5d008b2ed5c4
Mart556 <marthaamer123@gmail.com>
2025-11-11 16:15:19 +0200

    add sace area paddings to fix navbar overlap and header

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Slot } from 'expo-router';
 import NavBar from 'components/NavBar';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import Header from 'components/Header';
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <SafeAreaView style={{ flex: 1 }} edges={['top']}>
+    <SafeAreaView style={{ paddingTop: insets.top, paddingBottom: insets.bottom, flex: 1 }}>
       <Header />
       <Slot />
       <NavBar />
