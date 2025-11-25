344136141b2a0dd6a3859dbad3882c3a5cb5681e
robinristo78 <robin-risto.kivit@voco.ee>
2025-11-25 13:30:58 +0200

    fix: add theme-blue color to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-blue': '#128AEB',
+      }
+    },
   },
   plugins: [],
 };
