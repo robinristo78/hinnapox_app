db8a0d4193c09535549c1d48edf324f9d2e95562
JarlBurget <jarl-ivor.burget@voco.ee>
2025-12-02 12:37:28 +0200

    Esmane kaardi struktuur.

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+// Load stations from JSON file
+import stations from '../data/tanklad.json';
 
 const Map = () => {
-  return <Text className="text-2xl text-white">Map Page</Text>;
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: stations[0].lat,
+          longitude: stations[0].lon,
+          latitudeDelta: 1.2, // zoom level
+          longitudeDelta: 1.2,
+        }}
+      >
+        {stations.map((station) => (
+          <Marker
+            key={station.id}
+            coordinate={{
+              latitude: station.lat,
+              longitude: station.lon,
+            }}
+            title={`${station.brand_name} - ${station.name}`}
+            description={`${station.address}, ${station.city}`}
+          />
+        ))}
+      </MapView>
+    </View>
+  );
 };
 
 export default Map;
